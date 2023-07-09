d1930d53e1191c374fc0b5aa04c0ea50ed51e4f3
yorifuji <yorifuji@gmail.com>
2023-07-09 09:27:49 +0900

    can add favorite on video player

--- lib/ui/video_player/video_player_page.dart
+++ lib/ui/video_player/video_player_page.dart
@@ -1,26 +1,43 @@
 import 'package:buzz_recipe_viewer/model/search_hit.dart';
+import 'package:buzz_recipe_viewer/ui/video_player/video_player_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class VideoPlayerPage extends StatelessWidget {
+class VideoPlayerPage extends ConsumerWidget {
   const VideoPlayerPage({super.key, required this.searchHit});
 
   final SearchHit searchHit;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(videoPlayerViewModelProvider.notifier);
     final controller = YoutubePlayerController(
       initialVideoId: searchHit.objectID,
       flags: const YoutubePlayerFlags(
         captionLanguage: 'ja',
       ),
     );
-    return Padding(
-      padding: const EdgeInsets.only(top: 32),
-      child: YoutubePlayer(
-        controller: controller,
-        showVideoProgressIndicator: true,
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 32),
+        child: YoutubePlayer(
+          controller: controller,
+          showVideoProgressIndicator: true,
+        ),
       ),
+      onLongPress: () {
+        viewModel.addFavorite(searchHit);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              'お気に入りに追加しました',
+              style: TextStyle(fontSize: 12),
+            ),
+            duration: Duration(seconds: 1),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/ui/video_player/video_player_view_model.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
+part of 'video_player_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$videoPlayerViewModelHash() =>
+    r'955dd9858232f475738a52364058497a2c11c639';
+
+/// See also [VideoPlayerViewModel].
+@ProviderFor(VideoPlayerViewModel)
+final videoPlayerViewModelProvider =
+    AutoDisposeNotifierProvider<VideoPlayerViewModel, void>.internal(
+  VideoPlayerViewModel.new,
+  name: r'videoPlayerViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$videoPlayerViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$VideoPlayerViewModel = AutoDisposeNotifier<void>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
