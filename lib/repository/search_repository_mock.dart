9d435ec368da3f61e04ee38eed9670cc8f39f629
yorifuji <yorifuji@gmail.com>
2023-07-13 18:37:56 +0900

    mock data

--- lib/repository/search_repository_mock.dart
+++ lib/repository/search_repository_mock.dart
@@ -24,11 +24,36 @@ class SearchRepositoryMock implements SearchRepository {
           10,
           (index) => SearchHit(
             id: 'id${index + page}',
-            title: '【テストデータ${index + page}】タイトルタイトルタイトルタイトルタイトルタイトルタイトル',
-            description:
-                '説明が入ります\nhttps://github.com/yorifuji/buzz_recipe_viewer',
+            title: '一度食べたら忘れられない！家庭で作れる究極の美味しさ、超・至高の唐揚げレシピ',
+            description: '動画を見て楽しんで頂けましたら\n'
+                '高評価、コメントをお願いします！\n'
+                '\n'
+                '【超・至高の唐揚げ】\n'
+                '是非お試しください！\n'
+                '\n'
+                '大賞受賞しました！ありがとうございます！\n'
+                '★2022年第9回レシピ本大賞　料理部門・大賞★\n'
+                '\n'
+                '★今回のレシピはこちら↓\n'
+                'ーーーーーーーーーーーーーー\n'
+                '【超・至高の唐揚げ】\n'
+                '鶏もも肉...1枚(350g)\n'
+                'にんにく...1片\n'
+                '生姜...5g\n'
+                '醤油...大さじ3\n'
+                'みりん...大さじ1\n'
+                '酒...大さじ1\n'
+                '味の素...8振り\n'
+                'ナツメグ...5振り\n'
+                'オールスパイス...4振り\n'
+                'クミン...2振り\n'
+                'パウダー黒胡椒...4振り\n'
+                'サラダ油...底1cmぐらいの量\n'
+                '片栗粉...適量\n'
+                '☆カットレモンを添えて\n'
+                'ーーーーーーーーーーーーーー\n',
             published: '',
-            timestamp: 0,
+            timestamp: DateTime.now().millisecondsSinceEpoch ~/ 1000,
             views: 12345,
             likes: 12345,
             image:
