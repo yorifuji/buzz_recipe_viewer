51470059fe797568006c66efd25cd32de976437b
yorifuji <yorifuji@gmail.com>
2023-12-23 10:19:32 +0900

    refactor: Add missing variable initialization

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  final String x = 0;
   WidgetsFlutterBinding.ensureInitialized();
 
   final (_, isar, packageInfo, sharedPreferences) = await (
