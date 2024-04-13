4b6b6719c4b67d4c2c813bdb487cf96ab6397611
yorifuji <yorifuji@gmail.com>
2024-04-14 08:43:27 +0900

    refactor: Update NotoSansJP font path and usage

--- lib/provider/theme_data_provider.dart
+++ lib/provider/theme_data_provider.dart
@@ -1,3 +1,4 @@
+import 'package:buzz_recipe_viewer/gen/fonts.gen.dart';
 import 'package:buzz_recipe_viewer/provider/app_color_preference_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -10,5 +11,6 @@ ThemeData themeData(ThemeDataRef ref, {bool isDarkMode = false}) {
     useMaterial3: true,
     brightness: isDarkMode ? Brightness.dark : Brightness.light,
     colorSchemeSeed: ref.watch(appColorPreferenceProvider).color,
+    fontFamily: FontFamily.notoSansJP,
   );
 }
