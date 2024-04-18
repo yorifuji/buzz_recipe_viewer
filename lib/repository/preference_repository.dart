6521814d5d2bb14ba2be574bf60da16062df5b8d
yorifuji <yorifuji@gmail.com>
2024-04-18 18:42:27 +0900

    WIP: Generics

--- lib/repository/preference_repository.dart
+++ lib/repository/preference_repository.dart
@@ -98,3 +98,70 @@ class FakeIntPreference extends _$IntPreference
   @override
   int build(IntKey keyValue) => value;
 }
+
+// enum PreferenceKeys<T> {
+//   // bool
+//   useInternalPlayer('use_internal_player', true),
+//   shouldShowWalkthrough('should_show_walkthrough', true),
+//   // int
+//   themeMode('theme_mode', 0),
+//   locale('locale', 0),
+//   // String
+//   appColor('app_color', ''),
+//   ;
+
+//   const PreferenceKeys(this.key, this.defaultValue);
+//   final String key;
+//   final T defaultValue;
+// }
+
+// class PreferenceKey<T> {
+//   const PreferenceKey(this.key, this.defaultValue);
+//   final String key;
+//   final T defaultValue;
+// }
+
+// class PreferenceKeys {
+//   static const useInternalPlayer =
+//       PreferenceKey<bool>('use_internal_player', true);
+//   static const shouldShowWalkthrough =
+//       PreferenceKey<bool>('should_show_walkthrough', true);
+//   static const themeMode = PreferenceKey<int>('theme_mode', 0);
+//   static const locale = PreferenceKey<int>('locale', 0);
+//   static const appColor = PreferenceKey<String>('app_color', '');
+//   // dummy
+//   static const nullableBool = PreferenceKey<bool?>('nullable_bool', null);
+// }
+
+// @riverpod
+// class PreferenceNotifier<T> extends _$PreferenceNotifier {
+//   @override
+//   T build(PreferenceKey<T> keyValue) {
+//     if (keyValue.defaultValue is bool) {
+//       final value = ref.watch(sharedPreferencesProvider).getBool(keyValue.key);
+//       return value as T ?? keyValue.defaultValue;
+//     } else if (keyValue.defaultValue is String) {
+//       final value =
+//           ref.watch(sharedPreferencesProvider).getString(keyValue.key);
+//       return value as T ?? keyValue.defaultValue;
+//     } else if (keyValue.defaultValue is int) {
+//       final value = ref.watch(sharedPreferencesProvider).getInt(keyValue.key);
+//       return value as T ?? keyValue.defaultValue;
+//     } else {
+//       throw UnimplementedError();
+//     }
+//   }
+
+//   Future<void> update(T value) async {
+//     if (value is bool) {
+//       await ref.read(sharedPreferencesProvider).setBool(keyValue.key, value);
+//     } else if (value is String) {
+//       await ref.read(sharedPreferencesProvider).setString(keyValue.key, value);
+//     } else if (value is int) {
+//       await ref.read(sharedPreferencesProvider).setInt(keyValue.key, value);
+//     } else {
+//       throw UnimplementedError();
+//     }
+//     ref.invalidateSelf();
+//   }
+// }
