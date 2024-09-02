12e903f43e0d0fcb680d88ee9df50b44b9efaade
yorifuji <yorifuji@gmail.com>
2024-09-02 23:47:21 +0900

    refactor(ui): wrap IntroductionScreen in Scaffold

--- lib/ui/walkthrough/walkthrough_page.dart
+++ lib/ui/walkthrough/walkthrough_page.dart
@@ -16,68 +16,76 @@ class WalkthroughPage extends ConsumerWidget {
     final notificationAuthorizeStatus =
         ref.watch(notificationAuthorizeStatusProvider);
 
-    return IntroductionScreen(
-      pages: [
-        PageViewModel(
-          title: t.walkthrough.intro1.title,
-          body: t.walkthrough.intro1.body,
-          image: Padding(
-            padding: const EdgeInsets.only(top: 32),
-            child: Assets.images.intro1.image(),
-          ),
-        ),
-        PageViewModel(
-          title: t.walkthrough.intro2.title,
-          body: t.walkthrough.intro2.body,
-          image: Padding(
-            padding: const EdgeInsets.only(top: 32),
-            child: Assets.images.intro2.image(),
-          ),
-        ),
-        PageViewModel(
-          title: t.walkthrough.intro3.title,
-          bodyWidget: Column(
-            children: [
-              Text(t.walkthrough.intro3.body),
-              const SizedBox(height: 16),
-              switch (notificationAuthorizeStatus) {
-                AsyncData(:final value) => switch (value) {
-                    AuthorizationStatus.denied => ElevatedButton(
-                        onPressed: () async {
-                          await AppSettings.openAppSettings(
-                            type: AppSettingsType.notification,
-                          );
-                        },
-                        child:
-                            Text(t.walkthrough.intro3.openNotificationSettings),
-                      ),
-                    AuthorizationStatus.notDetermined => ElevatedButton(
-                        onPressed: () async {
-                          await NotificationService.requestPermission();
-                          ref.invalidate(notificationAuthorizeStatusProvider);
-                        },
-                        child: Text(t.walkthrough.intro3.requestPermission),
-                      ),
-                    _ => const SizedBox.shrink(),
+    return Scaffold(
+      body: SafeArea(
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+              title: t.walkthrough.intro1.title,
+              body: t.walkthrough.intro1.body,
+              image: Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: Assets.images.intro1.image(),
+              ),
+            ),
+            PageViewModel(
+              title: t.walkthrough.intro2.title,
+              body: t.walkthrough.intro2.body,
+              image: Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: Assets.images.intro2.image(),
+              ),
+            ),
+            PageViewModel(
+              title: t.walkthrough.intro3.title,
+              bodyWidget: Column(
+                children: [
+                  Text(t.walkthrough.intro3.body),
+                  const SizedBox(height: 16),
+                  switch (notificationAuthorizeStatus) {
+                    AsyncData(:final value) => switch (value) {
+                        AuthorizationStatus.denied => ElevatedButton(
+                            onPressed: () async {
+                              await AppSettings.openAppSettings(
+                                type: AppSettingsType.notification,
+                              );
+                            },
+                            child: Text(
+                              t.walkthrough.intro3.openNotificationSettings,
+                            ),
+                          ),
+                        AuthorizationStatus.notDetermined => ElevatedButton(
+                            onPressed: () async {
+                              await NotificationService.requestPermission();
+                              ref.invalidate(
+                                notificationAuthorizeStatusProvider,
+                              );
+                            },
+                            child: Text(t.walkthrough.intro3.requestPermission),
+                          ),
+                        _ => const SizedBox.shrink(),
+                      },
+                    _ => const Center(child: CircularProgressIndicator()),
                   },
-                _ => const Center(child: CircularProgressIndicator()),
-              },
-            ],
-          ),
-          decoration: const PageDecoration(bodyAlignment: Alignment.center),
+                ],
+              ),
+              decoration: const PageDecoration(bodyAlignment: Alignment.center),
+            ),
+          ],
+          showSkipButton: true,
+          showNextButton: false,
+          skip: Text(t.walkthrough.skip),
+          done: Text(t.walkthrough.done),
+          onDone: () {
+            ref
+                .read(
+                  boolPreferenceProvider(Preference.shouldShowWalkthrough)
+                      .notifier,
+                )
+                .update(false);
+          },
         ),
-      ],
-      showSkipButton: true,
-      showNextButton: false,
-      skip: Text(t.walkthrough.skip),
-      done: Text(t.walkthrough.done),
-      onDone: () {
-        ref
-            .read(
-              boolPreferenceProvider(Preference.shouldShowWalkthrough).notifier,
-            )
-            .update(false);
-      },
+      ),
     );
   }
 }
