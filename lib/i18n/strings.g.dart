dc118e54d9be1a63cfc4dab11b7fdf1f2a8734cf
yorifuji <yorifuji@gmail.com>
2024-09-08 19:01:14 +0900

    Update build timestamp in strings.g.dart

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 225 (112 per locale)
 ///
-/// Built on 2024-09-08 at 09:31 UTC
+/// Built on 2024-09-08 at 09:52 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
