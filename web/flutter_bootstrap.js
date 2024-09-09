1ae93aa331ee5cf1e33b9443939fcc83e4619855
yorifuji <yorifuji@gmail.com>
2024-09-09 18:10:19 +0900

    Add service worker settings to Flutter loader config

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -4,4 +4,7 @@
 const userConfig = {'canvasKitBaseUrl': 'canvaskit/'}
 _flutter.loader.load({
     config: userConfig,
+    serviceWorkerSettings: {
+      serviceWorkerVersion: {{flutter_service_worker_version}},
+    },
 })
