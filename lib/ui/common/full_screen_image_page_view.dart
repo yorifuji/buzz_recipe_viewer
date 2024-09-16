6d35aeb088b422bcc70981494b42236eb7c62c68
yorifuji <yorifuji@gmail.com>
2024-09-16 22:46:35 +0900

    Refactor fullscreen image dialog layout structure

--- lib/ui/common/full_screen_image_page_view.dart
+++ lib/ui/common/full_screen_image_page_view.dart
@@ -12,59 +12,44 @@ void showFullscreenImageDialog(
       Animation<double> animation,
       Animation<double> secondaryAnimation,
     ) {
-      return SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.black,
-          body: Stack(
-            children: [
-              PageView.builder(
-                controller: PageController(initialPage: initialIndex),
-                itemCount: imageUrls.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(buildContext).pop();
-                    },
-                    child: InteractiveViewer(
-                      minScale: 0.5,
-                      maxScale: 3,
-                      child: Center(
-                        child: Image.network(
-                          imageUrls[index],
-                          fit: BoxFit.contain,
-                          loadingBuilder: (
-                            BuildContext context,
-                            Widget child,
-                            ImageChunkEvent? loadingProgress,
-                          ) {
-                            if (loadingProgress == null) return child;
-                            return Center(
-                              child: CircularProgressIndicator(
-                                value: loadingProgress.expectedTotalBytes !=
-                                        null
-                                    ? loadingProgress.cumulativeBytesLoaded /
-                                        loadingProgress.expectedTotalBytes!
-                                    : null,
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                  );
+      return Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: PageView.builder(
+            controller: PageController(initialPage: initialIndex),
+            itemCount: imageUrls.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(buildContext).pop();
                 },
-              ),
-              Positioned(
-                top: 10,
-                left: 10,
-                child: IconButton(
-                  icon: const Icon(Icons.close, color: Colors.white),
-                  onPressed: () {
-                    Navigator.of(buildContext).pop();
-                  },
+                child: InteractiveViewer(
+                  minScale: 0.5,
+                  maxScale: 3,
+                  child: Center(
+                    child: Image.network(
+                      imageUrls[index],
+                      fit: BoxFit.contain,
+                      loadingBuilder: (
+                        BuildContext context,
+                        Widget child,
+                        ImageChunkEvent? loadingProgress,
+                      ) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                    loadingProgress.expectedTotalBytes!
+                                : null,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              );
+            },
           ),
         ),
       );
