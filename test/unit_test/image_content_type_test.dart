f7cf6871e715e50d0f48fd6ae2d443f6ee7fe8c5
yorifuji <yorifuji@gmail.com>
2024-09-21 19:53:38 +0900

    Update image content type tests for various cases

--- test/unit_test/image_content_type_test.dart
+++ test/unit_test/image_content_type_test.dart
@@ -3,51 +3,50 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('ContentType', () {
-    test('should return correct ContentType for supported extensions', () {
+    test('fromPath は正しい ContentType を返す', () {
       expect(ContentType.fromPath('image.jpg'), ContentType.jpeg);
-      expect(ContentType.fromPath('image.jpeg'), ContentType.jpeg);
-      expect(ContentType.fromPath('image.png'), ContentType.png);
-      expect(ContentType.fromPath('image.gif'), ContentType.gif);
-      expect(ContentType.fromPath('image.webp'), ContentType.webp);
-      expect(ContentType.fromPath('image.bmp'), ContentType.bmp);
+      expect(ContentType.fromPath('photo.png'), ContentType.png);
+      expect(ContentType.fromPath('animation.gif'), ContentType.gif);
+      expect(ContentType.fromPath('picture.webp'), ContentType.webp);
+      expect(ContentType.fromPath('graphic.bmp'), ContentType.bmp);
       expect(ContentType.fromPath('image.tiff'), ContentType.tiff);
-      expect(ContentType.fromPath('image.tif'), ContentType.tiff);
-      expect(ContentType.fromPath('image.svg'), ContentType.svg);
-      expect(ContentType.fromPath('image.heic'), ContentType.heic);
+      expect(ContentType.fromPath('vector.svg'), ContentType.svg);
+      expect(ContentType.fromPath('photo.heic'), ContentType.heic);
       expect(ContentType.fromPath('image.heif'), ContentType.heif);
-      expect(ContentType.fromPath('image.raw'), ContentType.raw);
-      expect(ContentType.fromPath('image.ico'), ContentType.ico);
+      expect(ContentType.fromPath('photo.raw'), ContentType.raw);
+      expect(ContentType.fromPath('icon.ico'), ContentType.ico);
     });
 
-    test('should throw UnsupportedError for unsupported extensions', () {
-      expect(
-        () => ContentType.fromPath('image.txt'),
-        throwsUnsupportedError,
-      );
-      expect(
-        () => ContentType.fromPath('image.doc'),
-        throwsUnsupportedError,
-      );
-      expect(
-        () => ContentType.fromPath('image.pdf'),
-        throwsUnsupportedError,
-      );
-    });
-
-    test('should handle case insensitivity in file extensions', () {
+    test('大文字小文字を区別しない', () {
       expect(ContentType.fromPath('image.JPG'), ContentType.jpeg);
-      expect(ContentType.fromPath('image.JPEG'), ContentType.jpeg);
-      expect(ContentType.fromPath('image.PNG'), ContentType.png);
-      expect(ContentType.fromPath('image.GIF'), ContentType.gif);
-      expect(ContentType.fromPath('image.WEBP'), ContentType.webp);
-      expect(ContentType.fromPath('image.BMP'), ContentType.bmp);
-      expect(ContentType.fromPath('image.TIFF'), ContentType.tiff);
-      expect(ContentType.fromPath('image.TIF'), ContentType.tiff);
-      expect(ContentType.fromPath('image.SVG'), ContentType.svg);
-      expect(ContentType.fromPath('image.HEIC'), ContentType.heic);
-      expect(ContentType.fromPath('image.HEIF'), ContentType.heif);
-      expect(ContentType.fromPath('image.RAW'), ContentType.raw);
-      expect(ContentType.fromPath('image.ICO'), ContentType.ico);
+      expect(ContentType.fromPath('photo.PNG'), ContentType.png);
+      expect(ContentType.fromPath('animation.GIF'), ContentType.gif);
+      expect(ContentType.fromPath('icon.ICO'), ContentType.ico);
+    });
+
+    test('複数のドットを含むファイル名を処理できる', () {
+      expect(ContentType.fromPath('image.backup.jpg'), ContentType.jpeg);
+      expect(ContentType.fromPath('photo.2023.png'), ContentType.png);
+      expect(ContentType.fromPath('icon.v2.ico'), ContentType.ico);
+    });
+
+    test('サポートされていない拡張子の場合は null を返す', () {
+      expect(ContentType.fromPath('document.pdf'), isNull);
+      expect(ContentType.fromPath('text.txt'), isNull);
+      expect(ContentType.fromPath('spreadsheet.xlsx'), isNull);
+    });
+
+    test('拡張子のない場合は null を返す', () {
+      expect(ContentType.fromPath('noextension'), isNull);
+    });
+
+    test('空文字の場合は null を返す', () {
+      expect(ContentType.fromPath(''), isNull);
+    });
+
+    test('ドットのみの場合は null を返す', () {
+      expect(ContentType.fromPath('.'), isNull);
+      expect(ContentType.fromPath('..'), isNull);
     });
   });
 }
