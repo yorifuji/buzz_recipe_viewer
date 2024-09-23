1b5011387f4f4c4fe4a150b6572a69d11f0c56b7
yorifuji <yorifuji@gmail.com>
2024-09-23 15:22:23 +0900

    Refactoring (#404)

--- lib/ui/common/image_slide_modal_sheet.dart
+++ lib/ui/common/image_slide_modal_sheet.dart
@@ -1,54 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 void showImageSlideModalSheet(
   BuildContext context,
   List<String> imageUrls, {
   int initialIndex = 0,
 }) {
-  showCupertinoModalBottomSheet<void>(
+  showModalBottomSheet<void>(
     context: context,
+    isScrollControlled: true,
+    showDragHandle: true,
+    useSafeArea: true,
     useRootNavigator: true,
     builder: (context) => Scaffold(
       // backgroundColor: Colors.black,
-      body: SafeArea(
-        child: PageView.builder(
-          controller: PageController(initialPage: initialIndex),
-          itemCount: imageUrls.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: InteractiveViewer(
-                minScale: 0.5,
-                maxScale: 3,
-                child: Center(
-                  child: Image.network(
-                    imageUrls[index],
-                    fit: BoxFit.contain,
-                    loadingBuilder: (
-                      BuildContext context,
-                      Widget child,
-                      ImageChunkEvent? loadingProgress,
-                    ) {
-                      if (loadingProgress == null) return child;
-                      return Center(
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                              ? loadingProgress.cumulativeBytesLoaded /
-                                  loadingProgress.expectedTotalBytes!
-                              : null,
-                          strokeWidth: 2,
-                        ),
-                      );
-                    },
-                  ),
+      body: PageView.builder(
+        controller: PageController(initialPage: initialIndex),
+        itemCount: imageUrls.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: InteractiveViewer(
+              minScale: 0.5,
+              maxScale: 3,
+              child: Center(
+                child: Image.network(
+                  imageUrls[index],
+                  fit: BoxFit.contain,
+                  loadingBuilder: (
+                    BuildContext context,
+                    Widget child,
+                    ImageChunkEvent? loadingProgress,
+                  ) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!
+                            : null,
+                        strokeWidth: 2,
+                      ),
+                    );
+                  },
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     ),
   );
