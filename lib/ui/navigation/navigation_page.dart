c86ca5bee976636cb575f3ea8a53f5dafcd7c469
yorifuji <yorifuji@gmail.com>
2024-10-23 20:02:07 +0900

    Add locale notifier to update tab titles dynamically

--- lib/ui/navigation/navigation_page.dart
+++ lib/ui/navigation/navigation_page.dart
@@ -1,4 +1,5 @@
 import 'package:buzz_recipe_viewer/i18n/strings.g.dart';
+import 'package:buzz_recipe_viewer/store/locale_notifier.dart';
 import 'package:buzz_recipe_viewer/ui/favorite/favorite_page.dart';
 import 'package:buzz_recipe_viewer/ui/recipe/recipe_page.dart';
 import 'package:buzz_recipe_viewer/ui/settings/settings_page.dart';
@@ -42,6 +43,10 @@ class NavigationPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentTab = useState<AppTab>(AppTab.recipe);
+    // to update tab titles when locale changes
+    ref.watch(
+      localeNotiferProvider,
+    );
 
     return PopScope(
       // スタックが存在しない場合はアプリを終了する（バックグラウンドに移動する）
