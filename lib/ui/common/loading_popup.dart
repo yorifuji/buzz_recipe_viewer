e44f6a7e1e4c4c5af9d73e7a2565ad7561f9bebb
yorifuji <yorifuji@gmail.com>
2024-12-12 08:46:16 +0900

    migration

--- lib/ui/common/loading_popup.dart
+++ lib/ui/common/loading_popup.dart
@@ -59,7 +59,7 @@ class _LoadingOverlay extends StatelessWidget {
             borderRadius: BorderRadius.circular(8),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.2),
+                color: Colors.black.withValues(alpha: 0.2),
                 blurRadius: 10,
                 offset: const Offset(0, 4),
               ),
