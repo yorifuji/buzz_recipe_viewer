2783e0cde42f5efa0a7cb40d48073c13a7c99cac
yorifuji <yorifuji@gmail.com>
2025-03-02 21:50:36 +0900

    Add jq installation and refactor TestFlight script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -10,6 +10,9 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
+# Install jq if not available
+which jq || HOMEBREW_NO_AUTO_UPDATE=1 brew install jq
+
 # Add rocket reaction to the triggering issue comment if available
 if [ -n "$CI_ISSUE_COMMENT_ID" ] && [ -n "$GITHUB_TOKEN" ] && [ -n "$CI_PULL_REQUEST_TARGET_REPO" ]; then
   echo "Adding rocket reaction to the triggering issue comment..."
@@ -22,38 +25,6 @@ if [ -n "$CI_ISSUE_COMMENT_ID" ] && [ -n "$GITHUB_TOKEN" ] && [ -n "$CI_PULL_REQ
   
   echo "Rocket reaction added to comment ID: $CI_ISSUE_COMMENT_ID"
 fi
-
-# Prepare TestFlight What to Test information
-WHAT_TO_TEST=""
-
-if [ -n "$CI_PULL_REQUEST_NUMBER" ] && [ -n "$GITHUB_TOKEN" ]; then
-  echo "Fetching PR information for TestFlight..."
-  PR_INFO=$(curl -s \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/repos/$CI_PULL_REQUEST_TARGET_REPO/pulls/$CI_PULL_REQUEST_NUMBER")
-  
-  PR_TITLE=$(printf '%s\n' "$PR_INFO" | jq -r '.title')
-  WHAT_TO_TEST="[PR#$CI_PULL_REQUEST_NUMBER]\n$PR_TITLE\n"
-fi
-
-COMMIT=$(git fetch --deepen 3 && git log -3 --pretty=format:"%s")
-
-WHAT_TO_TEST=${WHAT_TO_TEST}"[COMMIT]\n$COMMIT"
-
-# Only create TestFlight information if we're building for App Store
-if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
-  TESTFLIGHT_DIR_PATH="$CI_PRIMARY_REPOSITORY_PATH/TestFlight"
-  mkdir -p $TESTFLIGHT_DIR_PATH
-  echo "$WHAT_TO_TEST" > $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-  echo "TestFlight What to Test information prepared at $TESTFLIGHT_DIR_PATH"
-else
-  echo "Skipping TestFlight What to Test preparation as CI_APP_STORE_SIGNED_APP_PATH is not available"
-fi
-
-# Install jq if not available
-which jq || HOMEBREW_NO_AUTO_UPDATE=1 brew install jq
 
 # Get Flutter version from .fvmrc
 FLUTTER_VERSION=$(jq -r '.flutter' .fvmrc)
